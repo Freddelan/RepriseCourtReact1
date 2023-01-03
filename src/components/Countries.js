59e0d8ebabb23588b12a80e88317a194874c0ccd
Freddelan <happyrush@hotmail.com>
2023-01-03 23:30:26 +0100

    03.01.2023

---
+++ src/components/Countries.js
@@ -0,0 +1,43 @@
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Card from "./Card";
+
+const Countries = () => {
+  const [data, setData] = useState([]);
+  const [rangeValue, setRangeValue] = useState(36);
+  const radios = ["Africa", "America", "Asia", "Europe", "Oceanie"];
+  // Le useEffect se joue lorque le composant est monté
+  useEffect(() => {
+    axios
+      .get("https://restcountries.com/v3.1/all")
+      .then((res) => setData(res.data));
+  }, []);
+
+  return (
+    <div className="countries">
+      <ul className="radio-container">
+        <input
+          type="range"
+          min="1"
+          max="250"
+          defaultValue={rangeValue}
+          onChange={(e) => setRangeValue(e.target.value)}
+        />
+        {radios.map((continent) => (
+          <li>
+            <input type="radio" id={continent} />
+            <label htmlFor={[continent]}>{continent}</label>
+          </li>
+        ))}
+        <input type="radio" id="afrique" />
+      </ul>
+      <ul>
+        {data.slice(0, rangeValue).map((country, index) => (
+          <Card key={index} country={country} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Countries;
---
+++ src/pages/About.js
@@ -0,0 +1,55 @@
+import React from "react";
+import Navigation from "../components/Navigation";
+import Logo from "../components/Logo";
+
+const About = () => {
+  return (
+    <div>
+      <Logo />
+      <Navigation />
+      <h1>A propos</h1>
+      <br />
+      <p>
+        Lorem ipsum dolor, sit amet consectetur adipisicing elit. Magni vero
+        nihil alias, inventore nisi eveniet architecto deleniti cum, nobis odit
+        laborum tempore. Molestiae labore unde inventore molestias at atque
+        sequi culpa adipisci nam voluptates voluptas ipsum, blanditiis omnis
+        eaque id similique accusantium quaerat illum enim mollitia tempore quae
+        vel. Iusto nesciunt quidem delectus veniam provident quasi deleniti sed,
+        porro, magni, quod expedita excepturi pariatur quae natus impedit quis
+        blanditiis ipsa quos? Inventore facilis id fugit ducimus consequuntur
+        minus nemo provident animi nam. Quaerat ducimus inventore non ullam
+        culpa debitis beatae id numquam similique tempora rem corporis dolore,
+        perspiciatis sequi minima?
+      </p>
+      <br />
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo,
+        necessitatibus molestias consectetur vel alias at quos ipsa consequuntur
+        minus sunt similique possimus expedita delectus vitae? Provident
+        adipisci beatae amet perspiciatis rerum id nostrum? Est veniam ipsa aut
+        quaerat nobis doloremque ipsam, deserunt beatae aliquam vel amet nisi
+        aliquid neque maiores! Atque est enim reprehenderit nobis officia velit
+        voluptate aut expedita, vero eaque perspiciatis officiis nemo saepe
+        porro temporibus error consequuntur, veritatis autem amet! Maxime saepe
+        soluta necessitatibus facere, enim dignissimos eligendi quasi, repellat
+        hic fugit, at cumque odit voluptatem veritatis nulla commodi aliquid
+        reiciendis quis alias consequatur perspiciatis. Ratione est tempore
+        officiis natus eligendi dolor facere deserunt, tempora distinctio
+        praesentium vitae aliquam, quod aspernatur nobis sunt maiores, aperiam
+        cupiditate consectetur nesciunt nisi rem debitis explicabo. Perspiciatis
+        consequatur, quo exercitationem expedita molestias, quae nam nobis
+        perferendis nihil blanditiis deserunt unde nisi laborum ut minima, culpa
+        sunt corporis esse consectetur repudiandae labore reprehenderit atque!
+        Illo eos architecto mollitia optio consequuntur enim maiores tempore
+        aliquid non delectus obcaecati eum repellendus, soluta, unde facilis
+        aspernatur molestias aperiam minus quisquam in, praesentium impedit
+        consequatur eligendi! Voluptatem magni quaerat pariatur deserunt omnis
+        id vero nisi, commodi officiis saepe, minus molestias reiciendis velit
+        ipsum, rerum corrupti corporis!
+      </p>
+    </div>
+  );
+};
+
+export default About;
